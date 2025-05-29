1761fcc8a8da43dcf20b16bde9e094cf83203409
Gleison santos <gleisonsantos818@gmail.com>
2025-05-29 16:59:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,5 +8,18 @@ document.addEventListener('DOMContentLoaded', function() {
     menuBtn.addEventListener('click', function() {
         const nav = document.querySelector('nav');
         nav.classList.toggle('active');
+    
+    const themeBtn = document.getElementById('theme-toggle');
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    // Salva a preferência no localStorage
+    localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
+});
+
+// Carrega o tema salvo ao recarregar a página
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+}
+    
     });
 });
